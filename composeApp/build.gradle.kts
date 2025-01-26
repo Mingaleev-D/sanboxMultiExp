c1db93608e2099663c9d7db3492118f0e5bee5d3
Dinar <mingaleev.d92@mail.ru>
2025-01-26 21:53:34 +0300

    dependency

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -7,6 +7,11 @@ plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
+    //
+    //add
+    alias(libs.plugins.jetbrains.kotlin.serialization)
+    alias(libs.plugins.ksp)
+   // alias(libs.plugins.room)
 }
 
 kotlin {
@@ -33,6 +38,11 @@ kotlin {
         androidMain.dependencies {
             implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
+            //
+            //add dependencies
+            implementation(libs.koin.android)
+            implementation(libs.koin.androidx.compose)
+            implementation(libs.ktor.client.okhttp)
         }
         commonMain.dependencies {
             implementation(compose.runtime)
@@ -43,6 +53,28 @@ kotlin {
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodel)
             implementation(libs.androidx.lifecycle.runtime.compose)
+
+            //
+            //add dependencies
+            implementation(libs.jetbrains.compose.navigation)
+            implementation(libs.kotlinx.serialization.json)
+           // implementation(libs.androidx.room.runtime)
+           // implementation(libs.sqlite.bundled)
+            implementation(libs.koin.compose)
+            implementation(libs.koin.compose.viewmodel)
+            api(libs.koin.core)
+
+            implementation(libs.bundles.ktor)
+            implementation(libs.bundles.coil)
+        }
+        iosMain.dependencies {
+            //
+            //add dependencies
+            implementation(libs.ktor.client.darwin)
+        }
+
+        dependencies {
+           // ksp(libs.androidx.room.compiler)
         }
     }
 }
@@ -77,4 +109,3 @@ android {
 dependencies {
     debugImplementation(compose.uiTooling)
 }
-
